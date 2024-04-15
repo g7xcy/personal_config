78f8a3fbb831068465fe682f514c003fb8d4ba78
g7xcy <g7xcy@outlook.com>
2024-04-16 03:43:53 +0800

    Fixed the wrong name of fish.

---
+++ config.fish
@@ -0,0 +1,8 @@
+bif status is-interactive
+    # Commands to run in interactive sessions can go here
+    set -U PATH /home/frederica/.local/bin /home/frederica/bin $PATH
+    set -x COLORTERM truecolor
+    set -x fish_charset utf-8
+    abbr -a ws cd /mnt/e/Workspace
+    abbr -a lsa ls -al
+end
