376ffea411ebbdafe7e55228ea46a0e7c4c605a6
g7xcy <g7xcy@outlook.com>
2024-04-17 02:38:51 +0800

    Add abbr.

--- config.fish
+++ config.fish
@@ -1,8 +1,10 @@
-bif status is-interactive
+if status is-interactive
     # Commands to run in interactive sessions can go here
     set -U PATH /home/frederica/.local/bin /home/frederica/bin $PATH
     set -x COLORTERM truecolor
     set -x fish_charset utf-8
     abbr -a ws cd /mnt/e/Workspace
     abbr -a lsa ls -al
+    abbr -a gitc git commit
+    abbr -a gita git add
 end
